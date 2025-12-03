be69d066a51bed5f5df20cf7ebbb3ae565b3795b
jason.huang <20609724+doutv@users.noreply.github.com>
2025-12-03 10:20:56 +0800

    Add new contracts xBETH, xETH, xOKSOL, and xSOL with specified decimal implementations

---
+++ contracts/xOKSOL.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {StakedTokenV1} from "./StakedTokenV1.sol";
+
+contract xOKSOL is StakedTokenV1 {
+    function decimals() public pure override returns (uint8) {
+        return 9;
+    }
+}
